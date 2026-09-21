d95b24a74376060bcb7413484ebb3b4140168c40
NguyenHuuNinh <nguyenhuuninh11032006@gmail.com>
2026-09-21 09:59:01 +0700

    Create BusinessException.java

---
+++ BE_Java_Core/Session15/OrderManager/BusinessException.java
@@ -0,0 +1,7 @@
+package vn.edu.rikkei.session15.ordermanager;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
